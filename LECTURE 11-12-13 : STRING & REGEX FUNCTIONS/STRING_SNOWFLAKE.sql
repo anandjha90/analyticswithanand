5331419114ea4e49191c5e808b3e932b1299ea75
Anand Jha <anandjha_1990@yahoo.com>
2023-09-16 18:53:58 +0530

    Update STRING_SNOWFLAKE.sql

--- LECTURE 11-12-13 : STRING & REGEX FUNCTIONS/STRING_SNOWFLAKE.sql
+++ LECTURE 11-12-13 : STRING & REGEX FUNCTIONS/STRING_SNOWFLAKE.sql
@@ -1,5 +1,4 @@
 USE DATABASE DEMO_DATABASE;
-
 CREATE OR REPLACE TABLE AGENTS
    (	
      AGENT_CODE CHAR(6) NOT NULL PRIMARY KEY, 
@@ -13,8 +12,6 @@ CREATE OR REPLACE TABLE AGENTS
 INSERT INTO AGENTS VALUES ('A007', 'Ramasundar', 'Bangalore',0.15,'077-25814763', '');
 INSERT INTO AGENTS(AGENT_CODE,AGENT_NAME,WORKING_AREA) 
 VALUES ('A110', 'Anand', 'Germany');
-
-
 INSERT INTO AGENTS VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969', '');
 INSERT INTO AGENTS VALUES ('A008', 'Alford', 'New York', '0.12', '044-25874365', '');
 INSERT INTO AGENTS VALUES ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-45625874', '');
@@ -26,6 +23,9 @@ INSERT INTO AGENTS VALUES ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964', '
 INSERT INTO AGENTS VALUES ('A006', 'McDen', 'London', '0.15', '078-22255588', '');
 INSERT INTO AGENTS VALUES ('A004', 'Ivan', 'Torento', '0.15', '008-22544166', '');
 INSERT INTO AGENTS VALUES ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178', '');
+
+
+
 
 SELECT * FROM AGENTS;
 
@@ -39,7 +39,6 @@ select substr('Raja Ram',0,3);
 select substr('Raja Ram',3);
 
 select substring('ANAND KUMAR JHA', -7);
-
 ---sajkdsdjfsfbffbkjtqjUK
 
 --Example 2: Get the substring from a specific string by using table data
@@ -61,7 +60,6 @@ select cast('1.6845' as decimal(6,2));
 select '1.6845'::decimal(6,1);
 
 select cast('10-Sep-2021' as timestamp);
-
 -- When the provided precision is insufficient to hold the input value, the Snowflake CAST command raises an error as follows:
 select cast('123.12' as number(4,2));
 --Here, precision is set as 4 but the input value has a total of 5 digits, thereby raising the error.
@@ -82,6 +80,7 @@ SELECT TRIM('********T E S T I N G 1 2 3 4********','*') AS TRIMMED_SPACE;
 select ltrim('#000000123', '0#');
 select ltrim('#0000AISHWARYA', '0#');
 select ltrim('      ANAND JHA', '');
+
 
 --RTRIM
 select rtrim('$125.00', '0.');
@@ -104,9 +103,7 @@ SELECT LENGTH(trim('  Snowflake Space Remove  ')) as length_string;
 
 --concat
 select * from agents;
-
 SELECT CONCAT('KA', ', ', 'India') as state_country;
-
 SELECT *,concat(AGENT_CODE, '-', AGENT_NAME) AS agent_details from agents;
 
 --Snowflake CONCAT_WS Function
@@ -127,8 +124,6 @@ The || operator provides alternative syntax for CONCAT and requires at least two
 For example,
 */
 select 'Nested' || ' CONCAT' || ' example!' as Concat_operator;
-
-
 --Handling NULL Values in CONCAT function and the Concatenation operator
 --For both the CONCAT function and the concatenation operator,
 --if one or both strings are null, the result of the concatenation is null.
@@ -147,9 +142,7 @@ select reverse('Hello, world!');
 -- SPLIT
 select split('127.0.0.1', '.');
 SELECT SPLIT('ANAND-KUMAR-JHA','-');
-
 select  0, split_part('11.22.33', '.',  0);
-
 select split_part('aaa--bbb-BBB--ccc', '--',1);
 select split_part('aaa--bbb-BBB--ccc', '--',2);
 select split_part('aaa--bbb-BBB--ccc', '--',3);
@@ -159,8 +152,7 @@ SELECT split(AGENT_DETAILS, '-')
 FROM (
 SELECT *,concat(AGENT_CODE, '-', AGENT_NAME) AS agent_details 
   from agents );
-  
-
+ 
 SELECT lower('India Is My Country') as lwr_strng;
 SELECT UPPER('India Is My Country') as upr_strng;
 
@@ -169,5 +161,155 @@ SELECT UPPER('India Is My Country') as upr_strng;
 
 select REPLACE( '   ANAND KUMAR JHA   ' ,' ','*');
 select REPLACE( '   ANAND KUMAR JHA   ' ,' '); -- 
-
 SELECT REPLACE('   T  E S T I N G 1 2 3 4   ',' ')
+
+*********************************CODE DISCUSSED IN CLASS**************************
+SELECT SUBSTR('ANAND KUMAR JHA',0,5) AS FIRST_NAME;
+SELECT substr('ANAND KUMAR JHA',6,6) AS MIDDLE_NAME;
+SELECT substr('ANAND KUMAR JHA',-3,5) AS LAST_NAME;
+
+select substr('ANAND KUMAR JHA',1,1) || substr('ANAND KUMAR JHA',-3,1) as
+initial;
+
+select CONCAT(substr('ANAND KUMAR JHA',1,1),substr('ANAND KUMAR JHA',-3,1)) as
+initial;
+
+SELECT CONCAT('ANAND ',' ',2,3,4,NULL,' JHA') AS FULL_NAME;
+
+CREATE OR REPLACE TABLE AGENTS
+(
+AGENT_CODE CHAR(6) NOT NULL PRIMARY KEY,
+AGENT_NAME CHAR(40) ,
+WORKING_AREA CHAR(35),
+COMMISSION NUMBER(10,2) DEFAULT 0.05,
+PHONE_NO CHAR(15),
+COUNTRY VARCHAR2(25)
+);
+
+INSERT INTO AGENTS VALUES ('A007', 'Ramasundar', 'Bangalore',0.15,'077-
+25814763', '');
+INSERT INTO AGENTS VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969',
+'');
+INSERT INTO AGENTS VALUES ('A008', 'Alford', 'New York', '0.12', '044-
+25874365', '');
+INSERT INTO AGENTS VALUES ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-
+45625874', '');
+INSERT INTO AGENTS VALUES ('A010', 'Santakumar', 'Chennai', '0.14', '007-
+22388644', '');
+INSERT INTO AGENTS VALUES ('A012', 'Lucida', 'San Jose', '0.12', '044-
+52981425', '');
+INSERT INTO AGENTS VALUES ('A005', 'Anderson', 'Brisban', '0.13', '045-
+21447739', '');
+INSERT INTO AGENTS VALUES ('A001', 'Subbarao', 'Bangalore', '0.14', '077-
+12346674', '');
+INSERT INTO AGENTS VALUES ('A002', 'Mukesh', 'Mumbai', '0.11', '029-
+12358964', '');
+INSERT INTO AGENTS VALUES ('A006', 'McDen', 'London', '0.15', '078-
+22255588', '');
+INSERT INTO AGENTS VALUES ('A004', 'Ivan', 'Torento', '0.15', '008-22544166',
+'');
+INSERT INTO AGENTS VALUES ('A009', 'Benjamin', 'Hampshair', '0.11', '008-
+22536178', '');
+INSERT INTO AGENTS(AGENT_CODE,AGENT_NAME,WORKING_AREA)
+VALUES ('A110', 'Anand', 'Germany');
+
+SELECT * FROM AGENTS;
+
+SELECT CONCAT(AGENT_CODE,'-',AGENT_NAME) AS AGENT_DETAIL FROM AGENTS;
+SELECT CONCAT_WS('-',AGENT_CODE,AGENT_NAME) AS AGENT_DETAIL FROM AGENTS;
+
+SELECT CONCAT_WS('-', 'KA','India') as state_country;
+
+SELECT * FROM DEMO_DB.PUBLIC."txn_table_hold_mult_value" 
+WHERE PRODUCT_ID LIKE '%48%';
+
+SELECT CONCAT(STORE_ID,'-',PRODUCT_ID) AS STORE_PROD_PAIR 
+FROM DEMO_DB.PUBLIC."txn_table_hold_mult_value";
+
+SELECT CONCAT_WS('-',STORE_ID,PRODUCT_ID) AS STORE_PROD_PAIR 
+FROM DEMO_DB.PUBLIC."txn_table_hold_mult_value";
+
+SELECT LENGTH(' ANAND ') AS MY_NAME_LENGTH;
+
+select LTRIM('#00001AISHWARYA', '#0') AS LEFT_TRIMMED_STR;
+
+select LTRIM('#0001230AISHWARYA', '#0') AS LEFT_TRIMMED_STR;
+
+select RTRIM('AISHWARYA#0000', '#0')AS RIGHT_TRIMMED_STR
+select TRIM('#0000AISHWARYA#0000', '#0')AS TRIMMED_STR;
+
+select RTRIM ('$125.00', '0.');
+
+select TRIM('❄-❄ABC-❄-', '❄-') as trimmed_string;
+
+--Remove leading and trailing *
+SELECT TRIM('********T*E*S*T*I N G 1 2 3 4********','*') AS
+TRIMMED_SPACE;
+
+SELECT REVERSE('Hello, world!');
+SELECT REVERSE('16-09-2023');
+SELECT REVERSE('INDEPENDENCE') AS REVERSED_STRING;
+select replace('abcd', 'bc') as replaced_nothing_just_remove;
+select REPLACE( ' ANAND KUMAR JHA ' ,' ','*') AS REPLACING_BLANK_WITH_STAR;
+SELECT REPLACE(' T E S T I N G 1 2 3 4 ',' ');
+SELECT REPEAT('ANAND JHA ', 5);
+
+
+SELECT LOWER('ANAND KUMAR JHA') AS LOWER_CASE;
+SELECT UPPER('ANAND KUMAR JHA') AS UPPER_CASE;
+SELECT INITCAP('INDIA Is MY cOUntRY') AS TITLE;
+
+SELECT LOWER('BehaviorTriggeredAPAC_APP_ALL_ALL_RegisteredNotTransacted-Promocode_Wave1_EN_APAC_AU')
+AS LOWER_CASE_STRING;
+SELECT UPPER('BehaviorTriggeredAPAC_APP_ALL_ALL_RegisteredNotTransacted-Promocode_Wave1_EN_APAC_AU')
+AS UPPER_CASE_STRING;
+SELECT INITCAP('BehaviorTriggeredAPAC_APP_ALL_ALL_RegisteredNotTransacted-Promocode_Wave1_EN_APAC_AU')
+AS CAMEL_CASE_STRING;
+select * from agents;
+
+CREATE OR REPLACE VIEW AGENTS_VIEW AS
+select *,
+split_part(PHONE_NO,'-', 1) as COUNTRY_CODE ,
+split_part(PHONE_NO,'-', 2) as STD_NO 
+FROM AGENTS;
+
+SELECT * FROM AGENTS_VIEW;
+
+ create or replace table aj_persons
+(
+ NAME CHAR(10),
+ CHILDREN VARCHAR(30)
+);
+
+
+INSERT INTO AJ_PERSONS
+VALUES('Mark','Marky,Mark Jr,Maria'),('John','Johnny,Jane');
+SELECT * FROM AJ_PERSONS;
+
+SELECT SPLIT(CHILDREN,',') AS SPLITTED_CHILDREN FROM AJ_persons;
+SELECT split(AGENT_DETAILS, '-')
+ FROM (
+ SELECT *,concat(AGENT_CODE, '-', AGENT_NAME) AS
+ agent_details
+ from agents );
+
+ select TO_NUMBER(split_part('11.22.33', '.', 1)) as first_part ,
+ split_part('11.22.33', '.', 2) as sec_part;
+
+ --NO OF POSSIBLE SPLIT WHICH CAN HAPPEN
+SELECT ARRAY_SIZE(split('shivam panwar',' ')) as no_of_splits;
+
+ select 
+ split_part('ANAND KUMAR JHA', ' ', 1) as first_name ,
+ split_part('ANAND KUMAR JHA', ' ', 2) as middle_name,
+ split_part('ANAND KUMAR JHA', ' ', 3) as last_name,
+ split_part('ANAND KUMAR JHA', ' ', 4) as no_part_avaialble;
+
+select split_part('aaa--bbb-BBB--ccc', '--',2) as first_part;
+
+select split('anand-kumar-jha','-') as splitted_string;
+
+select * from AGENTS
+ where working_area LIKE '%e';
+
+
