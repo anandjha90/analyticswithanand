d294c2de95822b0ba22c6f401f9dd0ccc75c651e
Anand Jha <anandjha_1990@yahoo.com>
2024-08-30 21:42:08 +0530

    Update orc_data.sql

--- LECTURE 41 : WORKING WITH ORC DATA/orc_data.sql
+++ LECTURE 41 : WORKING WITH ORC DATA/orc_data.sql
@@ -1,3 +1,6 @@
+// For setup, you must create a database, table, and file format. See below for SQL Statements:
+
+
 //CREATE TABLE
 
 CREATE OR REPLACE TABLE SAMPLE_ORC(src VARIANT);
@@ -13,6 +16,12 @@ DESIGNATION VARCHAR(50));
 //CREATE A FILE FORMAT
 CREATE OR REPLACE FILE FORMAT orc_ff TYPE = 'ORC';
 
+// Querying the Stage
+// With Snowflake you have the ability to Query the data before you load it in. 
+// For this sample, we are going to look at the data and also show how we can specify certain columns to ingest 
+// into our Snowflake environment. 
+
+// I will start by querying my stage to check the files 
 ------------------------------------------------------------------------------------------
 --WORKING WITH THE STAGE
 ------------------------------------------------------------------------------------------
@@ -48,12 +57,21 @@ ALTER PIPE orc_demo_pipe refresh;
 //QUERY ALL FILES IN STAGE
 list @orc_demo_stage;
 
+// We can look at each record in the ORC file by using the below statement. Notice how we utilize the file format we created in the setup step. 
+// We can also query and utilize dot notation to specify which columns we need out of the ORC file. 
+// In this case, we will utilize _col2 (FIRSTNAME), _col3(LASTNAME), _col4(EMAIL), _col5(GENDER), and _col8(COUNTRY) as our desired columns. 
+
 //LOOK AT DATA IN THE ORC FILE
 SELECT t.$1 from @orc_demo_stage (file_format => orc_ff) t;
+
+//If you click on the first record you will see the below. This shows you what the record will look like and tells us what values we need to grab. 
 
 //QUERY TO GET DESIRED COLUMN NAMES
 SELECT t.$1:"_col2",t.$1:"_col3",t.$1:"_col4",t.$1:"_col5",t.$1:"_col8",t.$1:"_col11"
 from @orc_demo_stage (file_format => orc_ff) t;
+
+//After looking at the data we can choose which files or columns we’d like to load utilizing the same SELECT as we utilized above. 
+//In order to load data into the table, we need to run a COPY INTO statement to get the data in. 
 
 ------------------------------------------------------------------------------------------
 --LOADING THE DATA
@@ -69,3 +87,6 @@ from @orc_demo_stage (file_format => orc_ff) t
 
 //CHECK LOADED VALUES
 SELECT * FROM ORC_TEST_TABLE;
+
+
+// We have successfully loaded in all the data. This is a sample of how easy it is to work with ORC data in the Snowflake environment.
