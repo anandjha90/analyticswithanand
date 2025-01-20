1fbab6be99210cc8bdd5496ca7559c22b768a8d5
Anand Jha <anandjha_1990@yahoo.com>
2025-01-20 19:10:36 +0530

    Update ext_stg_aws_s3_data_upload.sql

--- LECTURE 45: Data_Unloading_With_Int_Ext_Stage/ext_stg_aws_s3_data_upload.sql
+++ LECTURE 45: Data_Unloading_With_Int_Ext_Stage/ext_stg_aws_s3_data_upload.sql
@@ -38,3 +38,25 @@ FROM (SELECT * FROM customer_data);
 -- Example for Table 2
 COPY INTO @s3_stage/sales_region_data.csv
 FROM (SELECT * FROM sales_region_data);
+
+-- Alternative Approach
+
+-- if staging details are not availble such as no roles and policies have been created abnd client have given aws access key then execute below steps
+CREATE OR REPLACE STAGE my_external_stage
+URL = 's3://czec-banking/DOWNLOAD_CSV/' -- path to S3 bucket folder
+--STORAGE_INTEGRATION = my_s3_integration
+CREDENTIALS = (
+    AWS_KEY_ID = '*******************'                         -- aws access key 
+    AWS_SECRET_KEY = '*******************'                     -- aws secret access key
+)
+FILE_FORMAT = csv_file_format;
+
+COPY INTO @my_external_stage/customer_data.csv -- copying data into external stage give proper file name with extension else it will create with data.csv(filename)
+FROM (SELECT * FROM customer_data)
+OVERWRITE=TRUE ; 
+
+-- if file already exists with teh same name it will overwrite it else if you have not use this property and trying to write to the same file you get an error
+
+COPY INTO @my_external_stage/sales_region_data.csv -- copying data into external stage give proper file name with extension else it will create with data.csv(filename)
+FROM (SELECT * FROM sales_region_data)
+OVERWRITE=TRUE ; 
