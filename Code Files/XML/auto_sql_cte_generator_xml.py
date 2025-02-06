da5649971f696dbdba343b530049cf899c6ca318
Anand Jha <anandjha_1990@yahoo.com>
2025-02-06 16:28:25 +0530

    Update auto_sql_cte_generator_xml.py

--- Code Files/XML/auto_sql_cte_generator_xml.py
+++ Code Files/XML/auto_sql_cte_generator_xml.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import xml.etree.ElementTree as ET
 import pandas as pd
+from collections import deque
 from datetime import datetime
 import re
 from io import BytesIO
@@ -40,12 +41,13 @@ def getToolData(filePath):
 
     return df
 
-def connectionDetails(filePath):
+
+def executionOrder(filePath):
     tree = ET.parse(filePath)
     root = tree.getroot()
     # Create a dictionary to map ToolID to its CTE name
-    tool_to_cte_map = {}
-    connections = {}
+    dependencies = []
+    tools = set()
 
     # Populate the map with connections data
     connectionRoot = root.find('Connections')
@@ -54,33 +56,65 @@ def connectionDetails(filePath):
         destination_tool_id = conn.find('Destination').get('ToolID')
 
         # Assign a CTE name to each ToolID (use a set to avoid duplication)
-        tool_to_cte_map[origin_tool_id] = f"CTE_{origin_tool_id}"
-        tool_to_cte_map[destination_tool_id] = f"CTE_{destination_tool_id}"
+        dependencies.append((destination_tool_id,origin_tool_id))
+        tools.add(origin_tool_id)
+        tools.add(destination_tool_id)
 
-        # Store the connection from origin to destination (handle multiple destinations per origin)
-        if origin_tool_id not in connections:
-            connections[origin_tool_id] = []
-        connections[origin_tool_id].append(destination_tool_id)
-    return tool_to_cte_map,connections
+    adj_list = {tool: [] for tool in tools}
+    in_degree = {tool: 0 for tool in tools}
+
+    for dependent,prerequisite in dependencies:
+        adj_list[prerequisite].append(dependent)
+        in_degree[dependent]+=1
+    queue = deque([tool for tool in tools if in_degree[tool]==0])
+    executionOrder = []
+    while queue:
+        tool = queue.popleft()
+        executionOrder.append(tool)
+        for dependent in adj_list[tool]:
+            in_degree[dependent]-=1
+            if in_degree[dependent]==0:
+                queue.append(dependent)
+    if len(executionOrder)!=len(tools):
+        raise ValueError("Cycle detected in dependencies. Execution order not possible")
+
+
+    return executionOrder
+
 
 
 # Function to iterate over each row and generate the correct CTE based on Plugin Name
-def generate_ctes_for_plugin(df):
-    # Iterate through each row in the DataFrame
-    for index, row in df.iterrows():
-        # Depending on Plugin Name, generate the appropriate CTE
-        if row['Plugin'] == 'AlteryxBasePluginsGui.AlteryxSelect.AlteryxSelect':
-            # Generate CTE for AlteryxSelect
-            df.loc[index, 'CTE'] = generate_cte_for_AlteryxSelect(row['Properties'])
-        elif row['Plugin'] == 'AlteryxSpatialPluginsGui.Summarize.Summarize':
-            # Generate CTE for Summarize
-            df.loc[index, 'CTE'] = generate_cte_for_Summarize(row['Properties'])
-        else:
-            df.loc[index, 'CTE'] = None  # If no logic for other Plugin Names
+def generate_ctes_for_plugin(df,executionOrder,parentMap):
+    cteResults = {}
+    for toolId in executionOrder:
+        toolRow = df[df['ToolID'] == toolId]
+
+        if toolRow is not None:
+            pluginName = toolRow.iloc[0]['Plugin']
+            properties= toolRow.iloc[0]['Properties']
+
+            if pluginName in plugin_functions:
+                previousToolId = parentMap[parentMap['Destination_ToolID'] == toolId]['Origin_ToolID'].squeeze() if not parentMap[parentMap['Destination_ToolID'] == toolId]['Origin_ToolID'].empty else None
+                cteGenerated = plugin_functions[pluginName](properties,previousToolId,toolId)
+                cteResults[toolId] = cteGenerated
+
+            elif pluginName =='AlteryxBasePluginsGui.Join.Join':
+                rightToolID = parentMap[(parentMap['Destination_ToolID'] == toolId) & (parentMap['Destination_Connection'] == 'Right')]['Origin_ToolID'].squeeze() if not parentMap[(parentMap['Destination_ToolID'] == toolId) & (parentMap['Destination_Connection'] == 'Right')]['Origin_ToolID'].empty else None
+                leftToolID = parentMap[(parentMap['Destination_ToolID'] == toolId) & (parentMap['Destination_Connection'] == 'Left')]['Origin_ToolID'].squeeze() if not parentMap[(parentMap['Destination_ToolID'] == toolId) & (parentMap['Destination_Connection'] == 'Left')]['Origin_ToolID'].empty else None
+                cteGenerated = generate_cte_for_Join(properties,rightToolID,leftToolID,toolId)
+                cteResults[toolId] = cteGenerated
+
+            else:
+                'No function available for plugin ', pluginName, toolId
+
+    df['CTE'] = df['ToolID'].map(cteResults)
 
     return df
 
-def generate_cte_for_AlteryxSelect(xml_data):
+
+
+
+def generate_cte_for_AlteryxSelect(xml_data,previousToolId,toolId):
     # Parse the XML data
     root = ET.fromstring(xml_data)
 
@@ -105,10 +139,10 @@ def generate_cte_for_AlteryxSelect(xml_data):
     # Generate the SQL CTE query string
     # Replace `YourTableName` with the actual table name in your context
     cte_query = f"""
-        SelectedFields AS (
+        {toolId} AS (
             SELECT
                 {', '.join(selected_columns)}
-            FROM YourTableName  
+            FROM {previousToolId}  
         )
         """
 
@@ -116,7 +150,7 @@ def generate_cte_for_AlteryxSelect(xml_data):
 
 
 # Function to parse the XML and generate SQL CTE for GroupBy and Aggregation
-def generate_cte_for_Summarize(xml_data):
+def generate_cte_for_Summarize(xml_data,previousToolId,toolId):
     root = ET.fromstring(xml_data)
 
     # Extract SummarizeFields
@@ -151,11 +185,11 @@ def generate_cte_for_Summarize(xml_data):
 
     # Generate the SQL CTE query string
     cte_query = f"""
-    SummarizedData AS (
+    {toolId} AS (
         SELECT
             {', '.join(group_by_fields)},  -- Group By Fields
             {', '.join(aggregate_fields)}  -- Aggregated Fields
-        FROM table_reference -- Use the previous CTE or current tool's CTE as the table
+        FROM {previousToolId}
         GROUP BY
             {', '.join(group_by_fields_before_rename)}  -- Group By Fields Before Rename
     )
@@ -164,6 +198,86 @@ def generate_cte_for_Summarize(xml_data):
     return cte_query
 
 
+def generate_cte_for_Join(xml_data,rightToolID,leftToolID,toolId):
+    root = ET.fromstring(xml_data)
+
+    # Extract JoinInfo for left and right
+    left_join_info = root.find('.//Configuration//JoinInfo[@connection="Left"]')
+    right_join_info = root.find('.//Configuration//JoinInfo[@connection="Right"]')
+
+    # Extract SelectFields for the selected fields
+    select_fields = root.findall('.//SelectConfiguration//SelectFields//SelectField')
+
+    # Collecting selected fields for SELECT statement
+    left_fields = []
+    right_fields = []
+
+    for field in select_fields:
+        field_name = field.get('field')
+        selected = field.get('selected') == 'True'  # True/False as string
+
+        if field_name.startswith("Left_") and selected:
+            left_fields.append(f'"{field_name}"')
+        elif field_name.startswith("Right_") and selected:
+            right_fields.append(f'"{field_name}"')
+
+    # Join condition between left and right tables
+    join_field_left = left_join_info.find('Field').get('field')
+    join_field_right = right_join_info.find('Field').get('field')
+
+    # Generating the CTE query in the format you specified
+    cte_query = f"""
+    {toolId} AS (
+        SELECT
+            {', '.join(left_fields)},  -- Left Selected Fields
+            {', '.join(right_fields)}  -- Right Selected Fields
+        FROM {leftToolID} AS LeftTable
+        INNER JOIN {rightToolID} AS RightTable
+        ON LeftTable."{join_field_left}" = RightTable."{join_field_right}"
+    )
+    """
+
+    return cte_query
+
+
+def connectionDetails(file,dfWithTool):
+    # Parse the XML data
+
+    tree = ET.parse(file)
+    root = tree.getroot()
+
+    # Initialize an empty list to store the connection data
+    data = []
+    connectionRoot = root.find('Connections')
+    for connection in connectionRoot.findall('.//Connection'):
+
+        # Get the connection name if it exists
+        connection_name = connection.get('name', None)
+
+        # Get the details of the origin and destination tools
+        origin_tool_id = connection.find('Origin').get('ToolID')
+        origin_connection = connection.find('Origin').get('Connection')
+
+        destination_tool_id = connection.find('Destination').get('ToolID')
+        destination_connection = connection.find('Destination').get('Connection')
+
+        # Store the data in the list
+        data.append({
+            'Connection_Name': connection_name,
+            'Origin_ToolID': origin_tool_id,
+            'Origin_Connection': origin_connection,
+            'Destination_ToolID': destination_tool_id,
+            'Destination_Connection': destination_connection
+        })
+
+    # Convert the data list into a pandas DataFrame
+    df = pd.DataFrame(data)
+
+    dfWithTool=dfWithTool[['ToolID', 'Plugin']]
+    df_with_tool_name = df.merge(dfWithTool, left_on='Destination_ToolID', right_on='ToolID', how='left')
+    df_with_tool_name.drop(columns='ToolID', inplace=True)
+
+    return df_with_tool_name
 
 if __name__ == "__main__":
     st.title('Alteryx Converter')
@@ -174,14 +288,26 @@ if __name__ == "__main__":
             st.write(getToolData(file))
 
             file.seek(0)
-            tool_to_cte_map,connections = connectionDetails(file)
+            executionOrder = executionOrder(file)
+
+            plugin_functions = {
+                'AlteryxBasePluginsGui.AlteryxSelect.AlteryxSelect': generate_cte_for_AlteryxSelect,
+                'LockInGui.LockInSelect.LockInSelect': generate_cte_for_AlteryxSelect,
+                'AlteryxSpatialPluginsGui.Summarize.Summarize': generate_cte_for_Summarize
+            }
+
+
 
             file.seek(0)
             df= getToolData(file)
             df['Source file'] = file.name
 
-            df = generate_ctes_for_plugin(df)
+            file.seek(0)
+            parentMap = connectionDetails(file,df)
+
+
+            df = generate_ctes_for_plugin(df,executionOrder,parentMap)
 
             st.write(df)
-            st.write(tool_to_cte_map)
-            st.write(connections)
+
+            st.write(parentMap)
